f6e822245e0cc9fde06810b5495783292532f684
Eslam358 <you@example.com>
2025-05-26 06:33:17 +0300

    add the cors

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 // const server = createServer(app);
-const io = new Server(server);
+// const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 const PORT = process.env.PORT || 3000
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
